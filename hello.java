c1e3f2bb3dd9f0a45686e94bf5e25df1bc4b73d1
ChungChan02 <cchan02@vt.edu>
2023-11-15 13:42:54 -0500

    Update hello.java
    Update hello.java

--- hello.java
+++ hello.java
@@ -1,1 +1,5 @@
-System.out.println("Hello, my name is Chung Chan")
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, my name is Chung Chan");
+    }
+}
